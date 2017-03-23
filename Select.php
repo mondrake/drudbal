042178567c577bc56373423c20d277024271a110
mondrake <mondrake@mondrake.org>
2017-03-23 17:26:35 +0100

    Update Select.php

--- Select.php
+++ Select.php
@@ -5,6 +5,6 @@ namespace Drupal\Driver\Database\drubal;
 use Drupal\Core\Database\Query\Select as QuerySelect;
 
 /**
- * MySQL implementation of \Drupal\Core\Database\Query\Select.
+ * DRUBAL implementation of \Drupal\Core\Database\Query\Select.
  */
 class Select extends QuerySelect { }
