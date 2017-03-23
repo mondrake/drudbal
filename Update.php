b1f76f6bc4933a2c26792e5d47c776f5b3b8000a
mondrake <mondrake@mondrake.org>
2017-03-23 17:27:42 +0100

    Update Update.php

--- Update.php
+++ Update.php
@@ -5,6 +5,6 @@ namespace Drupal\Driver\Database\drubal;
 use Drupal\Core\Database\Query\Update as QueryUpdate;
 
 /**
- * MySQL implementation of \Drupal\Core\Database\Query\Update.
+ * DRUBAL implementation of \Drupal\Core\Database\Query\Update.
  */
 class Update extends QueryUpdate { }
