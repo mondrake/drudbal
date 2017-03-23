0d6b7424cd71749d995d98919a7f84ac8e01d51d
mondrake <mondrake@mondrake.org>
2017-03-23 17:26:14 +0100

    Update Merge.php

--- Merge.php
+++ Merge.php
@@ -5,6 +5,6 @@ namespace Drupal\Driver\Database\drubal;
 use Drupal\Core\Database\Query\Merge as QueryMerge;
 
 /**
- * MySQL implementation of \Drupal\Core\Database\Query\Merge.
+ * DRUBAL implementation of \Drupal\Core\Database\Query\Merge.
  */
 class Merge extends QueryMerge { }
