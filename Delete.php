41e5fb0c634ba173a28974c7f6182ad09a2a9a1c
mondrake <mondrake@mondrake.org>
2017-03-23 17:25:38 +0100

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -5,6 +5,6 @@ namespace Drupal\Driver\Database\drubal;
 use Drupal\Core\Database\Query\Delete as QueryDelete;
 
 /**
- * MySQL implementation of \Drupal\Core\Database\Query\Delete.
+ * DRUBAL implementation of \Drupal\Core\Database\Query\Delete.
  */
 class Delete extends QueryDelete { }
