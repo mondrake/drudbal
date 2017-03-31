604f1d25cb6e58f978fea1f6bb74dd7f6c5bc341
mondrake <mondrake@mondrake.org>
2017-03-31 10:20:49 +0200

    docs

--- Update.php
+++ Update.php
@@ -6,5 +6,10 @@ use Drupal\Core\Database\Query\Update as QueryUpdate;
 
 /**
  * DRUBAL implementation of \Drupal\Core\Database\Query\Update.
+ *
+ * Note: there should not be db platform specific code here. Any tasks that
+ * cannot be managed by Doctrine DBAL should be added to driver specific code
+ * in Drupal\Driver\Database\drubal\DBALDriver\[driver_name] classes and
+ * execution handed over to there.
  */
 class Update extends QueryUpdate { }
--- Upsert.php
+++ Upsert.php
@@ -5,7 +5,12 @@ namespace Drupal\Driver\Database\drubal;
 use Drupal\Core\Database\Query\Upsert as QueryUpsert;
 
 /**
- * MySQL implementation of \Drupal\Core\Database\Query\Upsert.
+ * DRUBAL implementation of \Drupal\Core\Database\Query\Upsert.
+ *
+ * Note: there should not be db platform specific code here. Any tasks that
+ * cannot be managed by Doctrine DBAL should be added to driver specific code
+ * in Drupal\Driver\Database\drubal\DBALDriver\[driver_name] classes and
+ * execution handed over to there.
  */
 class Upsert extends QueryUpsert {
 
