3084f5ac30aa055556a50d207ea751089b3319e3
mondrake <mondrake@mondrake.org>
2017-04-12 19:46:59 +0200

    start work on Insert; looks like DBAL does not support mutiple insert statements and the INSERT INTO ... SELECT statement :(

--- drivers/lib/Drupal/Driver/Database/dbal/Insert.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Insert.php
@@ -14,6 +14,16 @@ use Drupal\Core\Database\Query\Insert as QueryInsert;
  */
 class Insert extends QueryInsert {
 
+  /**
+   * A DBAL query builder object.
+   *
+   * @var \Doctrine\DBAL\Query\QueryBuilder
+   */
+  protected $dbalQuery;
+
+  /**
+   * {@inheritdoc}
+   */
   public function execute() {
     if (!$this->preExecute()) {
       return NULL;
@@ -34,7 +44,28 @@ class Insert extends QueryInsert {
       $values = $this->fromQuery->getArguments();
     }
 
+if(in_array($this->table, ['test', 'test_people_copy', 'test_special_columns', 'mondrake_test'])) {
+    // DBAL does not support multiple insert statements. In such case, open a
+    // transaction and process separately each values set.
+    $sql = (string) $this;
+    if (count($this->insertValues) > 1) {
+      $transaction = $this->connection->startTransaction();
+    }
+    foreach ($this->insertValues as $insert_values) {
+      $max_placeholder = 0;
+      $values = [];
+      foreach ($insert_values as $value) {
+        $values[':db_insert_placeholder_' . $max_placeholder++] = $value;
+      }
+      $last_insert_id = $this->connection->query($sql, $values, $this->queryOptions);
+    }
+    if (count($this->insertValues) > 1) {
+      $transaction = NULL;
+    }
+}
+else {
     $last_insert_id = $this->connection->query((string) $this, $values, $this->queryOptions);
+}
 
     // Re-initialize the values array so that we can re-use this query.
     $this->insertValues = [];
@@ -42,6 +73,9 @@ class Insert extends QueryInsert {
     return $last_insert_id;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function __toString() {
     // Create a sanitized comment string to prepend to the query.
     $comments = $this->connection->makeComment($this->comments);
@@ -61,6 +95,20 @@ class Insert extends QueryInsert {
     $values = $this->getInsertPlaceholderFragment($this->insertValues, $this->defaultFields);
     $query .= implode(', ', $values);
 
+if(in_array($this->table, ['test', 'test_people_copy', 'test_special_columns', 'mondrake_test'])) {
+    $dbal_connection = $this->connection->getDbalConnection();
+    $prefixed_table = $this->connection->getPrefixedTableName($this->table);
+
+    // Use DBAL query builder to prepare the INSERT query.
+    $this->dbalQuery = $dbal_connection->createQueryBuilder()->insert($prefixed_table);
+
+    $max_placeholder = 0;
+    foreach ($insert_fields as $field) {
+      $this->dbalQuery->setValue($field, ':db_insert_placeholder_' . $max_placeholder++);
+    }
+debug($this->dbalQuery->getSQL());
+    return $comments . $this->dbalQuery->getSQL();
+}
     return $query;
   }
 
