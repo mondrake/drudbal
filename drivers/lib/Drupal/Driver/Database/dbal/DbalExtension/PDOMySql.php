c1b15cd9301ab8b0ed065817b0e368f645068fb7
mondrake <mondrake@mondrake.org>
2017-04-27 17:08:12 +0200

    move clientVersion from PDOMySql to Abstract

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOMySql.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOMySql.php
@@ -6,4 +6,12 @@ namespace Drupal\Driver\Database\dbal\DbalExtension;
  * Driver specific methods for pdo_mysql.
  */
 class PDOMySql extends AbstractMySqlExtension {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clientVersion() {
+    return $this->dbalConnection->getWrappedConnection()->getAttribute(\PDO::ATTR_CLIENT_VERSION);
+  }
+
 }
