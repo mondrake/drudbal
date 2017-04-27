9c1541991d7e31babb13f1f1c039c8d607c5b360
mondrake <mondrake@mondrake.org>
2017-04-27 16:01:27 +0200

    fix

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOMySql.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOMySql.php
@@ -5,5 +5,5 @@ namespace Drupal\Driver\Database\dbal\DbalExtension;
 /**
  * Driver specific methods for pdo_mysql.
  */
-class PDOMySql implements AbstractMySqlExtension {
+class PDOMySql extends AbstractMySqlExtension {
 }
