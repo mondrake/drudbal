3a6c53925fb62e67deec0d8e331f5489c85ece14
mondrake <mondrake@mondrake.org>
2017-05-01 12:32:12 +0200

    Cleanup Connection (#14)

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/AbstractMySqlExtension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/AbstractMySqlExtension.php
@@ -143,43 +143,7 @@ abstract class AbstractMySqlExtension implements DbalExtensionInterface {
   }
 
   /**
-   * @todo
-   */
-  public static function preConnectionOpen(array &$connection_options, array &$dbal_connection_options) {
-    if (isset($connection_options['_dsn_utf8_fallback']) && $connection_options['_dsn_utf8_fallback'] === TRUE) {
-      // Only used during the installer version check, as a fallback from utf8mb4.
-      $charset = 'utf8';
-    }
-    else {
-      $charset = 'utf8mb4';
-    }
-
-    // Character set is added to dsn to ensure PDO uses the proper character
-    // set when escaping. This has security implications. See
-    // https://www.drupal.org/node/1201452 for further discussion.
-    $connection_options['charset'] = $charset;
-    $dbal_connection_options['charset'] = $charset;
-
-    $dbal_connection_options['driverOptions'] += [
-      \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-      // So we don't have to mess around with cursors and unbuffered queries by default.
-      \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => TRUE,
-      // Make sure MySQL returns all matched rows on update queries including
-      // rows that actually didn't have to be updated because the values didn't
-      // change. This matches common behavior among other database systems.
-      \PDO::MYSQL_ATTR_FOUND_ROWS => TRUE,
-      // Because MySQL's prepared statements skip the query cache, because it's dumb.
-      \PDO::ATTR_EMULATE_PREPARES => TRUE,
-    ];
-    if (defined('\PDO::MYSQL_ATTR_MULTI_STATEMENTS')) {
-      // An added connection option in PHP 5.5.21 to optionally limit SQL to a
-      // single statement like mysqli.
-      $dbal_connection_options['driverOptions'] += [\PDO::MYSQL_ATTR_MULTI_STATEMENTS => FALSE];
-    }
-  }
-
-  /**
-   * @todo
+   * {@inheritdoc}
    */
   public static function postConnectionOpen(DbalConnection $dbal_connection, array &$connection_options, array &$dbal_connection_options) {
     // Force MySQL to use the UTF-8 character set. Also set the collation, if a
--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/DbalExtensionInterface.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/DbalExtensionInterface.php
@@ -2,7 +2,53 @@
 
 namespace Drupal\Driver\Database\dbal\DbalExtension;
 
+use \Doctrine\DBAL\Connection as DbalConnection;
+
 /**
  * Provides an interface for Dbal extensions.
  */
-interface DbalExtensionInterface { }
+interface DbalExtensionInterface {
+
+  /**
+   * Prepares opening the DBAL connection.
+   *
+   * Allows driver/db specific options to be set before opening the DBAL
+   * connection.
+   *
+   * @param array $connection_options
+   *   An array of connection options, valid for Drupal database connections.
+   * @param array $dbal_connection_options
+   *   An array of connection options, valid for DBAL database connections.
+   *
+   * @see \Drupal\Core\Database\Connection
+   * @see \Doctrine\DBAL\Connection
+   */
+  public static function preConnectionOpen(array &$connection_options, array &$dbal_connection_options);
+
+  /**
+   * Executes actions after having opened the DBAL connection.
+   *
+   * Allows driver/db specific options to be executed once the DBAL connection
+   * has been opened.
+   *
+   * @param \Doctrine\DBAL\Connection $dbal_connection
+   *   The DBAL connection.
+   * @param array $connection_options
+   *   An array of connection options, valid for Drupal database connections.
+   * @param array $dbal_connection_options
+   *   An array of connection options, valid for DBAL database connections.
+   *
+   * @see \Drupal\Core\Database\Connection
+   * @see \Doctrine\DBAL\Connection
+   */
+  public static function postConnectionOpen(DbalConnection $dbal_connection, array &$connection_options, array &$dbal_connection_options);
+
+  /**
+   * Returns the version of the database client.
+   *
+   * This is the low-level database client version, like mysqlind in mysql
+   * drivers.
+   */
+  public function clientVersion();
+
+}
