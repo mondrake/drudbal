d91458ec91e5e343002347907f61127e911451b9
mondrake <mondrake@mondrake.org>
2017-05-09 12:11:14 +0200

    Put full travis conf back, added a @todo for SQLParserUtils::getPlaceholderPositions failure

--- drivers/lib/Drupal/Driver/Database/dbal/Statement/MysqliDbalStatement.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Statement/MysqliDbalStatement.php
@@ -109,11 +109,11 @@ class MysqliDbalStatement implements \IteratorAggregate, StatementInterface {
     }
 
     $statement = strtr($statement, [
-       '\\\\' => "]]]]DOUBLESLASHESDRUDBAL[[[[",
+       '\\\\' => "]]]]DOUBLESLASHESDRUDBAL[[[[",  // @todo SQLParserUtils::getPlaceholderPositions issue
     ]);
     list($positional_statement, $positional_params, $positional_types) = SQLParserUtils::expandListParameters($statement, $params, []);
     $positional_statement = strtr($positional_statement, [
-       "]]]]DOUBLESLASHESDRUDBAL[[[[" => '\\\\',
+       "]]]]DOUBLESLASHESDRUDBAL[[[[" => '\\\\',  // @todo SQLParserUtils::getPlaceholderPositions issue
     ]);
     $this->_conn = $dbh->getDbalConnection()->getWrappedConnection()->getWrappedResourceHandle();
     $this->_stmt = $this->_conn->prepare($positional_statement);
@@ -133,11 +133,11 @@ class MysqliDbalStatement implements \IteratorAggregate, StatementInterface {
    * {@inheritdoc}
    */
   public function execute($args = [], $options = []) {
-    $statement = strtr($this->queryString, [
+    $statement = strtr($this->queryString, [  // @todo SQLParserUtils::getPlaceholderPositions issue
        '\\\\' => "]]]]DOUBLESLASHESDRUDBAL[[[[",
     ]);
     list($positional_statement, $positional_args, $positional_types) = SQLParserUtils::expandListParameters($statement, $args, []);
-    $positional_statement = strtr($positional_statement, [
+    $positional_statement = strtr($positional_statement, [  // @todo SQLParserUtils::getPlaceholderPositions issue
        "]]]]DOUBLESLASHESDRUDBAL[[[[" => '\\\\',
     ]);
 
