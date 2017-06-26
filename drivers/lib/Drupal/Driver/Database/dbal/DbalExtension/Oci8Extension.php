a61be7adc487f37550d43f42fd86437c937bc102
mondrake <mondrake@mondrake.org>
2017-06-26 18:05:16 +0200

    test caching

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/Oci8Extension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/Oci8Extension.php
@@ -369,7 +369,7 @@ if ($exc_class !== 'Doctrine\\DBAL\\Exception\\TableNotFoundException') {
    * {@inheritdoc}
    */
   public function delegateChangeField(&$primary_key_processed_by_extension, DbalSchema $dbal_schema, $drupal_table_name, $field_name, $field_new_name, array $drupal_field_new_specs, array $keys_new_specs, array $dbal_column_options) {
-    $sql = 'ALTER TABLE {' . $drupal_table_name . '} CHANGE `' . $field_name . '` `' . $field_new_name . '` ' . $dbal_column_options['columnDefinition'];
+    $sql = 'ALTER TABLE {' . $drupal_table_name . '} MODIFY (' . $field_name . ' ' . $dbal_column_options['columnDefinition'] . ')';
     $this->connection->query($sql);
     return TRUE;
   }
