f66fcc36f97ca115d1d740904e7dc32a2a25cfdf
mondrake <mondrake@mondrake.org>
2017-06-27 16:07:38 +0200

    xx

--- drivers/lib/Drupal/Driver/Database/dbal/Schema.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Schema.php
@@ -883,6 +883,7 @@ class Schema extends DatabaseSchema {
    */
   protected function dbalExecuteSchemaChange(DbalSchema $to_schema) {
     foreach ($this->dbalSchema()->getMigrateToSql($to_schema, $this->dbalPlatform) as $sql) {
+error_log($sql);
       $this->connection->getDbalConnection()->exec($sql);
     }
     $this->dbalSetCurrentSchema($to_schema);
