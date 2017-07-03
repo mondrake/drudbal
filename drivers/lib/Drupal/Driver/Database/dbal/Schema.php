1252cc7a43ce7809d1783896452de3c9282c3d52
mondrake <mondrake@mondrake.org>
2017-07-03 14:27:04 +0200

    try using quoted table names to enforce Drupal lower case standard for tables

--- drivers/lib/Drupal/Driver/Database/dbal/Schema.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Schema.php
@@ -132,14 +132,6 @@ class Schema extends DatabaseSchema {
       }
     }
 
-    // Add check constraints for unsigned.
-//    foreach ($table['fields'] as $field_name => $field) {
-//      if (!empty($field['unsigned']) && (bool) $field['unsigned'] === TRUE) {
-//        $sql = "ALTER TABLE " . $this->tableName($name) . " ADD CONSTRAINT " . $field_name . '_CHECK (' . $field_name . ' >= 0)';
-//        $this->connection->getDbalConnection()->exec($sql);
-//      }
-//    }
-
   }
 
   /**
