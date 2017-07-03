c750de7f5d0afa4120792ea128075c3b2298940c
mondrake <mondrake@mondrake.org>
2017-07-03 16:14:54 +0200

    tests

--- drivers/lib/Drupal/Driver/Database/dbal/Schema.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Schema.php
@@ -913,9 +913,12 @@ class Schema extends DatabaseSchema {
     $individually_prefixed_tables = $this->connection->getUnprefixedTablesMap();
     $default_prefix = $this->connection->tablePrefix();
     $default_prefix_length = strlen($default_prefix);
+error_log(var_export($default_prefix . ' ' . $default_prefix_length);
 
     $tables = [];
+error_log(var_export($this->dbalExtension->delegateListTableNames(), TRUE));
     foreach ($this->dbalExtension->delegateListTableNames() as $table_name) {
+error_log('Table: ' . $table_name);
       // Take into account tables that have an individual prefix.
       if (isset($individually_prefixed_tables[$table_name])) {
         $prefix_length = strlen($this->connection->tablePrefix($individually_prefixed_tables[$table_name]));
@@ -944,7 +947,9 @@ class Schema extends DatabaseSchema {
     // Convert the table expression from its SQL LIKE syntax to a regular
     // expression and escape the delimiter that will be used for matching.
     $table_expression = str_replace(['%', '_'], ['.*?', '.'], preg_quote($table_expression, '/'));
+error_log(var_export($tables, TRUE));
     $tables = preg_grep('/^' . $table_expression . '$/i', $tables);
+error_log(var_export($tables, TRUE));
 
     return $tables;
   }
