0f446da9bb45c1bbd91be17c144dfab06bcef838
mondrake <mondrake@mondrake.org>
2017-07-27 21:57:46 +0200

    debugging

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/Oci8Extension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/Oci8Extension.php
@@ -633,17 +633,17 @@ SQL;
     $to_schema = clone $current_schema;
     $dbal_table = $to_schema->getTable($this->tableName($drupal_table_name));
     $dbal_column = $dbal_table->getColumn($field_name); // @todo getdbfieldname
-error_log('pre : ' . var_export($dbal_column->getNotnull(), TRUE));
-$change_nullability = TRUE;
-if (array_key_exists('not null', $drupal_field_new_specs) && $drupal_field_new_specs['not null'] == $dbal_column->getNotnull()) {
-  $change_nullability = FALSE;
-}
 
-$sql = "ALTER TABLE " . $this->tableName($drupal_table_name) . " MODIFY ($field NUMBER(10) ";
-if ($change_nullability) {
-  $sql .= $drupal_field_new_specs['not null'] ? 'NOT NULL' : 'NULL';
-}
-$sql .= ")";
+    $change_nullability = TRUE;
+    if (array_key_exists('not null', $drupal_field_new_specs) && $drupal_field_new_specs['not null'] == $dbal_column->getNotnull()) {
+      $change_nullability = FALSE;
+    }
+
+    $sql = "ALTER TABLE " . $this->tableName($drupal_table_name) . " MODIFY ($field NUMBER(10) ";
+    if ($change_nullability) {
+      $sql .= $drupal_field_new_specs['not null'] ? 'NOT NULL' : 'NULL';
+    }
+    $sql .= ")";
     $this->connection->query($sql);
 
 
