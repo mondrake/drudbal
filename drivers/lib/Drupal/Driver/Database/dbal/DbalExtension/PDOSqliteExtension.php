c3f630038491658a9ff0eb38cadbe57c5ce26920
mondrake <mondrake@mondrake.org>
2018-01-05 14:34:28 +0100

    test

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
@@ -235,7 +235,7 @@ class PDOSqliteExtension extends AbstractExtension {
             $this->query('ATTACH DATABASE :database AS :prefix', [':database' => $connection_options['database'], ':prefix' => $prefix]);
           }
           else {*/
-            $this->getDbalConnection()->exec('ATTACH DATABASE ' . $connection_options['database'] . '-' . $connection_options['prefix']['default'] . '-' . $prefix . ' AS ' . $prefix);
+            $this->connection->query('ATTACH DATABASE :database AS :prefix', [':database' => $connection_options['database'] . '-' . $connection_options['prefix']['default'] . '-' . $prefix, ':prefix' => $prefix]);
 //          }
         }
 
