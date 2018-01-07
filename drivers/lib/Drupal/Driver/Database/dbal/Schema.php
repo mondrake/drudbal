03ef21658c9c0a24c2418a7a0c0629bd700cd840
mondrake <mondrake@mondrake.org>
2018-01-07 19:18:47 +0100

    test4

--- drivers/lib/Drupal/Driver/Database/dbal/Schema.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Schema.php
@@ -84,6 +84,7 @@ class Schema extends DatabaseSchema {
    * {@inheritdoc}
    */
   public function createTable($name, $table) {
+error_log('table: ' . $name);
     if ($this->tableExists($name)) {
       throw new SchemaObjectExistsException(t('Table @name already exists.', ['@name' => $name]));
     }
