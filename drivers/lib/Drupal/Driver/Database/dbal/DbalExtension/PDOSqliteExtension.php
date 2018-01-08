19a91d723987d4c7ecac8cc5d114b6b76c391815
mondrake <mondrake.org@gmail.com>
2018-01-08 12:28:14 +0100

    Update PDOSqliteExtension.php

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
@@ -189,6 +189,7 @@ class PDOSqliteExtension extends AbstractExtension {
    * {@inheritdoc}
    */
   public static function preConnectionOpen(array &$connection_options, array &$dbal_connection_options) {
+$connection_options['database'] = '/home/travis/drupal8/sqlite-drudbal';
     $dbal_connection_options['path'] = $connection_options['database'] === ':memory:' ? 'file::memory:?cache=shared' : $connection_options['database'];
     if (isset($connection_options['prefix']['default']) && $connection_options['prefix']['default'] !== '') {
       $dbal_connection_options['path'] .= '-' . $connection_options['prefix']['default'];
