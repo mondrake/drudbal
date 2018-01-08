38da459741b936390ca07a91130fbdca34df4edc
mondrake <mondrake@mondrake.org>
2018-01-08 10:01:40 +0100

    test

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/AbstractExtension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/AbstractExtension.php
@@ -159,7 +159,7 @@ error_log(var_export(['instance ' . $this->debugId, $drudbal_connection->getConn
   public function getDbFullQualifiedTableName($drupal_table_name) {
     $options = $this->connection->getConnectionOptions();
     $prefix = $this->connection->tablePrefix($drupal_table_name);
-error_log(var_export([$drupal_table_name, $prefix], true));
+error_log(var_export([$drupal_table_name, $prefix, $options, $options['database'] . '.' . $this->getDbTableName($prefix, $drupal_table_name)], true));
     return $options['database'] . '.' . $this->getDbTableName($prefix, $drupal_table_name);
   }
 
