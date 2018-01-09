17800ddf831f2482ad1f63ee80c3625aaed6c6fd
mondrake <mondrake@mondrake.org>
2018-01-09 10:19:15 +0100

    test

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
@@ -162,7 +162,7 @@ class PDOSqliteExtension extends AbstractExtension {
     // that the prefix is no longer relevant.
     if (in_array($context, ['indexExists', 'dropIndex'])) {
       $dbal_table = $dbal_schema->getTable($this->tableName($drupal_table_name));
-if ($drupal_table_name === 'node_field_data') throw new \Exception(var_export([$drupal_table_name, $dbal_table, $index_name, array_keys($dbal_table->getIndexes())], TRUE));
+if ($drupal_table_name === 'node_field_data') throw new \Exception(var_export([$drupal_table_name, $index_name, array_keys($dbal_table->getIndexes())], TRUE));
       foreach ($dbal_table->getIndexes() as $index) {
         $index_full_name = $index->getName();
         $matches = [];
