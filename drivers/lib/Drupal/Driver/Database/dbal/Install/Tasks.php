77af62b15cebcc30d2a49583f5be9b28a7b50bba
mondrake <mondrake@mondrake.org>
2018-07-25 19:29:14 +0200

    10

--- drivers/lib/Drupal/Driver/Database/dbal/Install/Tasks.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Install/Tasks.php
@@ -157,11 +157,11 @@ class Tasks extends InstallTasks {
     $form['advanced_options']['port']['#type'] = 'hidden';
 
     // In functional tests, the 'dbal_url' database key needs to be rebuilt.
-    if (empty($database['dbal_url']) && isset($database['database'])) {
+/*    if (empty($database['dbal_url']) && isset($database['database'])) {
       $connection_options = $database;
       $connection_options['driver'] = 'dbal';
       $database['dbal_url'] = DruDbalConnection::createUrlFromConnectionOptions($connection_options);
-    }
+    }*/
 
     // Add a Dbal URL entry field.
     $form['dbal_url'] = [
