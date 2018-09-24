e8836f8323d5a5f98c66d82b22d95e29eb355331
mondrake <mondrake.org@gmail.com>
2018-09-24 16:29:36 +0200

    3

--- drivers/lib/Drupal/Driver/Database/dbal/Schema.php
+++ drivers/lib/Drupal/Driver/Database/dbal/Schema.php
@@ -397,7 +397,7 @@ class Schema extends DatabaseSchema {
         $this->dbalExecuteSchemaChange($to_schema);
       }
       catch (DbalDriverException $e) {
-        $this->dbalSchemaForceReload();
+        //$this->dbalSchemaForceReload();
       }
       $current_schema = $this->dbalSchema();
       $to_schema = clone $current_schema;
