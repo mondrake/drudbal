872642f97812c054a6ca6f8d9d0321e16d6a7792
mondrake <mondrake.org@gmail.com>
2018-09-26 12:46:51 +0200

    19

--- drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
+++ drivers/lib/Drupal/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
@@ -823,8 +823,6 @@ class PDOSqliteExtension extends AbstractExtension {
           }
         }
       }
-//global $xxx;
-//if ($xxx) throw new \Exception(var_export($new_schema, TRUE));
       $this->alterTable($drupal_table_name, $old_schema, $new_schema, $mapping);
     }
     return TRUE;
