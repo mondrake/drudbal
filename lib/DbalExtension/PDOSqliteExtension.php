7efd27100742d06e39cb90b6d80268813efd873d
mondrake <mondrake.org@gmail.com>
2020-03-13 15:48:59 +0100

    6

--- lib/DbalExtension/PDOSqliteExtension.php
+++ lib/DbalExtension/PDOSqliteExtension.php
@@ -782,7 +782,7 @@ class PDOSqliteExtension extends AbstractExtension {
       // version.
       $dbal_type = $this->connection->schema()->getDbalColumnType($drupal_field_specs);
       $dbal_column_options = $this->connection->schema()->getDbalColumnOptions('addField', $field_name, $dbal_type, $drupal_field_specs);
-      $query = 'ALTER TABLE {' . $drupal_table_name . '} ADD ' . $field_name . ' ' . $dbal_column_options['columnDefinition'];
+      $query = 'ALTER TABLE {' . $drupal_table_name . '} ADD "' . $field_name . '" ' . $dbal_column_options['columnDefinition'];
       $this->connection->query($query);
     }
     else {
