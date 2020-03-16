c4566e603596ec9b9b5ac3bec4fe138c2ab20251
mondrake <mondrake.org@gmail.com>
2020-03-16 12:10:36 +0100

    7

--- lib/Connection.php
+++ lib/Connection.php
@@ -157,7 +157,7 @@ class Connection extends DatabaseConnection {
     $matches = [];
     preg_match_all('/{(\S*)}/', $sql, $matches, PREG_SET_ORDER, 0);
     foreach ($matches as $match) {
-      $table = str_replace(['{', '}'], '', $match[1]);
+      $table = str_replace(['{', '}'], ['', ''], $match[1]);
       if (isset($this->dbTables['{' . $table . '}'])) {
         continue;
       }
