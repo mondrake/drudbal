7568d9403ab69fb5f5fcfc4e5d97bd1578c7dd34
mondrake <mondrake.org@gmail.com>
2020-06-22 11:55:40 +0200

    2

--- tests/src/Kernel/DatabaseExceptionWrapperTest.php
+++ tests/src/Kernel/DatabaseExceptionWrapperTest.php
@@ -5,6 +5,7 @@ namespace Drupal\Tests\drudbal\Kernel;
 use Drupal\Core\Database\DatabaseExceptionWrapper;
 use Drupal\Core\Database\Database;
 use Drupal\KernelTests\KernelTestBase;
+use Doctrine\DBAL\DBALException;
 
 /**
  * Tests exceptions thrown by queries.
@@ -18,40 +19,23 @@ class DatabaseExceptionWrapperTest extends KernelTestBase {
    * Tests deprecation of Connection::prepare.
    */
   public function testPrepare() {
-    $this->merkTestSkipped('It\'s deprecated, Jim.');
+    $this->markTestSkipped('It\'s deprecated, Jim.');
   }
 
   /**
    * Tests deprecation of Connection::prepareQuery.
    */
   public function testPrepareQuery() {
-    $this->merkTestSkipped('It\'s deprecated, Jim.');
+    $this->markTestSkipped('It\'s deprecated, Jim.');
   }
 
   /**
    * Tests Connection::prepareStatement exceptions.
    */
   public function testPrepareStatement() {
-    $this->expectException(\PDOException::class);
+    $this->expectException(DBALException::class);
     $stmt = Database::getConnection()->prepareStatement('bananas', []);
     $stmt->execute();
-  }
-
-  /**
-   * Tests the expected database exception thrown for inexistent tables.
-   */
-  public function testQueryThrowsDatabaseExceptionWrapperException() {
-    $connection = Database::getConnection();
-    try {
-      $connection->query('SELECT * FROM {does_not_exist}');
-      $this->fail('Expected PDOException, none was thrown.');
-    }
-    catch (DatabaseExceptionWrapper $e) {
-      $this->pass('Expected DatabaseExceptionWrapper was thrown.');
-    }
-    catch (\Exception $e) {
-      $this->fail("Thrown exception is not a DatabaseExceptionWrapper:\n" . (string) $e);
-    }
   }
 
 }
