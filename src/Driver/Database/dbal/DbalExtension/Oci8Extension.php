141dda4fb58aaf7888c7ad3165c226886ca5b80b
mondrake <mondrake.org@gmail.com>
2020-12-16 09:48:08 +0100

    16-2

--- src/Driver/Database/dbal/DbalExtension/Oci8Extension.php
+++ src/Driver/Database/dbal/DbalExtension/Oci8Extension.php
@@ -585,6 +585,7 @@ if ($this->getDebugging()) error_log($query . ' : ' . var_export($args, TRUE));
    * {@inheritdoc}
    */
   public function delegateTableExists(&$result, $drupal_table_name) {
+dump(['delegateTableExists', $drupal_table_name]);
     // The DBAL Schema manager is quite slow here.
     // Instead, we try to select from the table in question.  If it fails,
     // the most likely reason is that it does not exist.
@@ -593,6 +594,7 @@ if ($this->getDebugging()) error_log($query . ' : ' . var_export($args, TRUE));
       $result = TRUE;
     }
     catch (\Exception $e) {
+dump(['delegateTableExists exception', $e]);
       $result = FALSE;
     }
     return TRUE;
