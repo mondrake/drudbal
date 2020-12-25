c2d26fcd5bc9f0b81b6a9fff6f6fea8a78a9f368
mondrake <mondrake.org@gmail.com>
2020-12-25 17:08:08 +0100

    25-14

--- TODO.md
+++ TODO.md
@@ -2,3 +2,4 @@ TODO
 
 * Connection::createUrlFromConnectionOptions does not export the module name
 * new identifier management class
+* move travisci stuff
---
+++ tests/github/drupal_patch.sh
@@ -0,0 +1,11 @@
+#2657888 Add Date function support in DTBNG
+curl https://www.drupal.org/files/issues/2657888-18.patch | git apply -v
+
+#2992274 Installer tests fail if contrib driver hides database credentials form fields
+curl https://www.drupal.org/files/issues/2020-11-23/2992274-13.patch | git apply -v
+
+#3110546 Allow contributed modules (mostly database drivers) to override tests in core
+git apply -v drudbal_staging/tests/travis_ci/alt-fix.patch
+
+#3189880 Use square brackets syntax in expressions
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/187.diff | git apply -v
