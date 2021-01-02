c9dc92ef65a90d9e34fff9b626a0c565c0bd0818
mondrake <mondrake.org@gmail.com>
2021-01-02 10:27:36 +0100

    Update Oci8Extension.php

--- src/Driver/Database/dbal/DbalExtension/Oci8Extension.php
+++ src/Driver/Database/dbal/DbalExtension/Oci8Extension.php
@@ -53,6 +53,7 @@ class Oci8Extension extends AbstractExtension {
    * Destructs an Oci8 extension object.
    */
   public function __destruct() {
+dump($this->tempTables);
     foreach ($this->tempTables as $db_table) {
       $this->dbalConnection->exec("DROP TABLE $db_table");
     }
