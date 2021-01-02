97f96a1bc5d66008625125fe860b08bb5e2dc274
mondrake <mondrake.org@gmail.com>
2021-01-02 08:42:26 +0100

    0201-1

---
+++ tests/src/Kernel/QueryTemporaryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\Tests\drudbal\Kernel;
+
+use Drupal\KernelTests\Core\Database\DatabaseTestBase;
+
+/**
+ * Tests the temporary query functionality.
+ *
+ * @group Database
+ */
+class QueryTemporaryTest extends DatabaseTestBase {
+
+  /**
+   * Confirms that temporary tables work.
+   */
+  public function testTemporaryQuery() {
+    $connection = Database::getConnection();
+
+    // Now try to run two temporary queries in the same request.
+    $table_name_test = $connection->queryTemporary('SELECT name FROM {test}', []);
+    $table_name_task = $connection->queryTemporary('SELECT pid FROM {test_task}', []);
+
+    $this->assertEquals($connection->select('test')->countQuery()->execute()->fetchField(), $connection->select($table_name_test)->countQuery()->execute()->fetchField(), 'A temporary table was created successfully in this request.');
+    $this->assertEquals($connection->select('test_task')->countQuery()->execute()->fetchField(), $connection->select($table_name_task)->countQuery()->execute()->fetchField(), 'A second temporary table was created successfully in this request.');
+
+    // Check that leading whitespace and comments do not cause problems
+    // in the modified query.
+    $sql = "
+      -- Let's select some rows into a temporary table
+      SELECT name FROM {test}
+    ";
+    $table_name_test = $connection->queryTemporary($sql, []);
+    $this->assertEqual($connection->select('test')->countQuery()->execute()->fetchField(), $connection->select($table_name_test)->countQuery()->execute()->fetchField(), 'Leading white space and comments do not interfere with temporary table creation.');
+  }
+
+}
---
+++ tests/src/Kernel/SchemaTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Drupal\Tests\drudbal\Kernel;
+
+use Drupal\KernelTests\Core\Database\SchemaTest as SchemaTestBase;
+
+/**
+ * Tests table creation and modification via the schema API.
+ *
+ * @group Database
+ */
+class SchemaTest extends SchemaTestBase {
+
+  /**
+   * Tests the findTables() method.
+   */
+  public function testFindTables() {
+    // We will be testing with three tables, two of them using the default
+    // prefix and the third one with an individually specified prefix.
+    // Set up a new connection with different connection info.
+    $connection_info = Database::getConnectionInfo();
+
+    // Add per-table prefix to the second table.
+    $new_connection_info = $connection_info['default'];
+    $new_connection_info['prefix']['test_2_table'] = $new_connection_info['prefix']['default'] . '_shared_';
+    Database::addConnectionInfo('test', 'default', $new_connection_info);
+    Database::setActiveConnection('test');
+    $test_schema = Database::getConnection()->schema();
+
+    // Create the tables.
+    $table_specification = [
+      'description' => 'Test table.',
+      'fields' => [
+        'id'  => [
+          'type' => 'int',
+          'default' => NULL,
+        ],
+      ],
+    ];
+    $test_schema->createTable('test_1_table', $table_specification);
+    $test_schema->createTable('test_2_table', $table_specification);
+    $test_schema->createTable('test_3_table', $table_specification);
+
+    // Check the "all tables" syntax.
+    $tables = $test_schema->findTables('%');
+    sort($tables);
+    $expected = [
+      // The 'config' table is added by
+      // \Drupal\KernelTests\KernelTestBase::containerBuild().
+      'config',
+      'test_1_table',
+      // This table uses a per-table prefix, yet it is returned as un-prefixed.
+      'test_2_table',
+      'test_3_table',
+    ];
+    $this->assertEqual($tables, $expected, 'All tables were found.');
+
+    // Check the restrictive syntax.
+    $tables = $test_schema->findTables('test_%');
+    sort($tables);
+    $expected = [
+      'test_1_table',
+      'test_2_table',
+    ];
+    $this->assertEqual($tables, $expected, 'Two tables were found.');
+
+    // Go back to the initial connection.
+    Database::setActiveConnection('default');
+  }
+
+  /**
+   * Tests handling of uppercase table names.
+   */
+  public function testUpperCaseTableName() {
+    $table_name = 'UPPER_CASE';
+
+    // Create the tables.
+    $table_specification = [
+      'description' => 'Test table.',
+      'fields' => [
+        'id'  => [
+          'type' => 'int',
+          'default' => NULL,
+        ],
+      ],
+    ];
+    $this->schema->createTable($table_name, $table_specification);
+
+    $this->assertTrue($this->schema->tableExists($table_name), 'Table with uppercase table name exists');
+    $this->assertContains($table_name, $this->schema->findTables('%'));
+    $this->assertTrue($this->schema->dropTable($table_name), 'Table with uppercase table name dropped');
+  }
+
+}
