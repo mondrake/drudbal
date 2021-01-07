191ad2fd9bd21c13d068bdcce3e68b9cd37e8f0e
mondrake <mondrake.org@gmail.com>
2021-01-07 23:15:28 +0100

    Adjust after d.o. #3189880 Use square brackets syntax in expressions (#210)

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -9,8 +9,5 @@ curl https://www.drupal.org/files/issues/2020-11-23/2992274-13.patch | git apply
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
 git apply -v drudbal_staging/tests/travis_ci/alt-fix.patch
 
-#3189880 Use square brackets syntax in expressions
-curl https://git.drupalcode.org/project/drupal/-/merge_requests/187.diff | git apply -v
-
 #3190285 entityQueryAggregate does not escape the field
-curl https://www.drupal.org/files/issues/2020-12-30/3190285-5-test-only.patch | git apply -v
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/209.diff | git apply -v
