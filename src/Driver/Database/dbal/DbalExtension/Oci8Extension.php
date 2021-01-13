43435974e9cfd9bdde718e716a674d85ccc1ef7d
mondrake <mondrake.org@gmail.com>
2021-01-13 17:20:38 +0100

    1301-13

--- src/Driver/Database/dbal/DbalExtension/Oci8Extension.php
+++ src/Driver/Database/dbal/DbalExtension/Oci8Extension.php
@@ -824,7 +824,7 @@ dump(['dbal_column' => $dbal_column, 'temp_column' => $temp_column]);
 //    $sql = str_replace("NUMBER(10) NOT NULL CHECK (\"age\">= 0)", "DEFAULT NULL", $sql);
     $column_definition = str_replace("\"{$dbal_column->getName()}\"", "\"$temp_column\"", $dbal_column_options['columnDefinition']);
     if ($not_null) {
-      $column_definition = str_replace("NOT NULL ", "", $column_definition);
+      $column_definition = str_replace("NOT NULL ", "NULL ", $column_definition);
     }
     $sql[] = "ALTER TABLE $db_table ADD \"$temp_column\" $column_definition";
     $sql[] = "UPDATE $db_table SET \"$temp_column\" = \"{$dbal_column->getName()}\"";
