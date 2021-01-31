ab9c3e04c472ec22dcc98b712063355d22d1cf20
mondrake <mondrake.org@gmail.com>
2021-01-31 14:35:01 +0100

    f1

--- tests/src/Kernel/SchemaTest.php
+++ tests/src/Kernel/SchemaTest.php
@@ -247,48 +247,4 @@ $this->counter = 0;
     }
   }
 
-  public function testSchemaChangeFieldDefaultInitial() {
-$this->counter = 0;
-$this->connection->getDbalExtension()->setDebugging(TRUE);
-    $field_specs = [
-      ['type' => 'int', 'size' => 'normal', 'not null' => FALSE],
-      ['type' => 'int', 'size' => 'normal', 'not null' => TRUE, 'initial' => 1, 'default' => 17],
-      ['type' => 'float', 'size' => 'normal', 'not null' => FALSE],
-      ['type' => 'float', 'size' => 'normal', 'not null' => TRUE, 'initial' => 1, 'default' => 7.3],
-      ['type' => 'numeric', 'scale' => 2, 'precision' => 10, 'not null' => FALSE],
-      ['type' => 'numeric', 'scale' => 2, 'precision' => 10, 'not null' => TRUE, 'initial' => 1, 'default' => 7],
-    ];
-
-    foreach ($field_specs as $i => $old_spec) {
-      foreach ($field_specs as $j => $new_spec) {
-        if ($i === $j) {
-          // Do not change a field into itself.
-          continue;
-        }
-        $this->assertFieldChange($old_spec, $new_spec);
-      }
-    }
-
-    $field_specs = [
-      ['type' => 'varchar_ascii', 'length' => '255'],
-      ['type' => 'varchar', 'length' => '255'],
-      ['type' => 'text'],
-      ['type' => 'blob', 'size' => 'big'],
-    ];
-
-    foreach ($field_specs as $i => $old_spec) {
-      foreach ($field_specs as $j => $new_spec) {
-        if ($i === $j) {
-          // Do not change a field into itself.
-          continue;
-        }
-        // Note if the serialized data contained an object this would fail on
-        // Postgres.
-        // @see https://www.drupal.org/node/1031122
-        $this->assertFieldChange($old_spec, $new_spec, serialize(['string' => "This \n has \\\\ some backslash \"*string action.\\n"]));
-      }
-    }
-
-  }
-
 }
