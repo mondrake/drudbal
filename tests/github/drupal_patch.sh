e544329c5b27d5707f1643119e2c914870b7d1b2
mondrake <mondrake.org@gmail.com>
2021-02-03 18:07:52 +0100

    Update drupal_patch.sh

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -4,7 +4,7 @@
 curl https://www.drupal.org/files/issues/2020-11-23/2992274-13.patch | git apply -v
 
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
-git apply -v drudbal_staging/tests/github/alt-fix.patch
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
 
 #3191623 Views aggregate queries do not escape the fields
 curl https://www.drupal.org/files/issues/2021-01-08/3190285-5-test-only.patch | git apply -v
