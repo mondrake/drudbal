9baa1321baeeb22cde46402bcd60ec630c36b0bf
mondrake <mondrake.org@gmail.com>
2021-05-11 00:34:19 +0200

    Two

--- src/Driver/Database/dbal/Connection.php
+++ src/Driver/Database/dbal/Connection.php
@@ -376,6 +376,14 @@ class Connection extends DatabaseConnection {
   /**
    * {@inheritdoc}
    */
+  public function queryTemporary($query, array $args = [], array $options = []) {
+    @trigger_error('Connection::queryTemporary() is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. There is no replacement. See https://www.drupal.org/node/3211781', E_USER_DEPRECATED);
+    return '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function driver() {
     return 'dbal';
   }
