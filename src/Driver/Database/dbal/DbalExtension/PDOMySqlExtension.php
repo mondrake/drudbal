c32e921e1363e8288194207c5c915ba924463b42
mondrake <mondrake.org@gmail.com>
2021-07-19 16:24:49 +0200

    Update PDOMySqlExtension.php

--- src/Driver/Database/dbal/DbalExtension/PDOMySqlExtension.php
+++ src/Driver/Database/dbal/DbalExtension/PDOMySqlExtension.php
@@ -26,12 +26,14 @@ class PDOMySqlExtension extends AbstractMySqlExtension {
       // Because MySQL's prepared statements skip the query cache, because it's
       // dumb.
       \PDO::ATTR_EMULATE_PREPARES => TRUE,
+      // Limit SQL to a single statement like mysqli.
+      \PDO::MYSQL_ATTR_MULTI_STATEMENTS => FALSE,
+      // Convert numeric values to strings when fetching. In PHP 8.1,
+      // \PDO::ATTR_EMULATE_PREPARES now behaves the same way as non emulated
+      // prepares and returns integers. See https://externals.io/message/113294
+      // for further discussion.
+      \PDO::ATTR_STRINGIFY_FETCHES => TRUE,
     ];
-    if (defined('\PDO::MYSQL_ATTR_MULTI_STATEMENTS')) {
-      // An added connection option in PHP 5.5.21 to optionally limit SQL to a
-      // single statement like mysqli.
-      $dbal_connection_options['driverOptions'] += [\PDO::MYSQL_ATTR_MULTI_STATEMENTS => FALSE];
-    }
   }
 
   /**
