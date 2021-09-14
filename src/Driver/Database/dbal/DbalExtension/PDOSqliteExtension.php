b42cfbcfc89e203bad3f9c12efef589d1b89e3b3
mondrake <mondrake.org@gmail.com>
2021-09-14 11:07:28 +0200

    2

--- src/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
+++ src/Driver/Database/dbal/DbalExtension/PDOSqliteExtension.php
@@ -87,7 +87,7 @@ class PDOSqliteExtension extends AbstractExtension {
     if ($drudbal_connection->getConnectionOptions()['database'] === ':memory:') {
       return;
     }
-dump(['A', $drudbal_connection]);
+dump(['A', $drudbal_connection->getConnectionOptions()]);
     // Attach databases per prefix.
     $prefixes = [];
     $connection_options = $drudbal_connection->getConnectionOptions();
@@ -105,6 +105,7 @@ dump(['A', $drudbal_connection]);
       }
       $prefixes[$key] = $prefix;
     }
+dump(['A', $prefixes, $this->attachedDatabases]);
     $this->connection->setPrefixPublic($prefixes);
   }
 
