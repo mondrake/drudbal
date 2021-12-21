a41d4998279a18df244dd40895d391d5df532b4a
mondrake <mondrake.org@gmail.com>
2021-12-21 15:45:57 +0100

    Update drupal_patch.sh

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -1,13 +1,16 @@
 #!/bin/sh -e
 
 #2992274 Installer tests fail if contrib driver hides database credentials form fields
-curl https://www.drupal.org/files/issues/2020-11-23/2992274-13.patch | git apply -v
+#curl https://www.drupal.org/files/issues/2020-11-23/2992274-13.patch | git apply -v
 
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
 
 #3191623 Views aggregate queries do not escape the fields
-curl https://git.drupalcode.org/project/drupal/-/merge_requests/785.diff | git apply -v
+#curl https://git.drupalcode.org/project/drupal/-/merge_requests/785.diff | git apply -v
+
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/1147.diff | git apply -v
+
 
 #ls -al drudbal_staging/tests/github/drupal_patch.sh
 #git apply -v drudbal_staging/tests/github/test.patch
