58a3eb389c6bc71f2ea69e3eff7e17eaca0aa2da
mondrake <mondrake.org@gmail.com>
2022-01-02 18:24:34 +0100

    11

--- src/Driver/Database/dbal/Truncate.php
+++ src/Driver/Database/dbal/Truncate.php
@@ -17,6 +17,16 @@ class Truncate extends QueryTruncate {
   /**
    * {@inheritdoc}
    */
+  public function __construct(Connection $connection, string $table, array $options = []) {
+    // @todo Remove the __construct in Drupal 11.
+    // @see https://www.drupal.org/project/drupal/issues/3256524
+    parent::__construct($connection, $table, $options);
+    unset($this->queryOptions['return']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function execute() {
     // Allow DBAL extension to process commands before a DDL TRUNCATE.
     if (!$this->connection->inTransaction()) {
