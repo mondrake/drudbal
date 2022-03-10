2f49da05930b89dd3ced3da388ff7a75e9f256a8
mondrake <mondrake.org@gmail.com>
2022-03-10 22:26:32 +0100

    CI fixes

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -13,4 +13,4 @@ curl https://git.drupalcode.org/project/drupal/-/merge_requests/785.diff | git a
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/1626.diff | git apply -v
 
 #ls -al drudbal_staging/tests/github/drupal_patch.sh
-git apply -v drudbal_staging/tests/github/test.patch
+#git apply -v drudbal_staging/tests/github/test.patch
