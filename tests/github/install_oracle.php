0a13e207bd91e0d87185c40b771503ca81320438
mondrake <mondrake.org@gmail.com>
2022-08-10 08:07:06 +0200

    test

--- tests/github/install_oracle.php
+++ tests/github/install_oracle.php
@@ -17,4 +17,21 @@ $class_loader = require_once $root_path . '/autoload.php';
 $tmpConnection = DriverManager::getConnection(["url" => "oci8://system:oracle@0.0.0.0:1521/XE"]);
 $tmpConnection->executeStatement('CREATE USER DRUDBAL IDENTIFIED BY ORACLE');
 $tmpConnection->executeStatement('GRANT DBA TO DRUDBAL');
-$tmpConnection->close();
+//$tmpConnection->close();
+
+$table = new Table('"tester"');
+$table->addColumn('"id"', Types::INTEGER);
+$table->addColumn('"name"', Types::STRING);
+$table->addColumn('"test_field"', Types::STRING);
+
+$schemaManager = $tmpConnection->createSchemaManager();
+$schemaManager->dropAndCreateTable($table);
+
+$current_schema = $schemaManager->createSchema();
+$to_schema = clone $current_schema;
+$to_schema->getTable('"tester"')->dropColumn('"test_field"');
+$schema_diff = (new Comparator())->compareSchemas($current_schema, $to_schema);
+foreach ($schema_diff->toSql($tmpConnection->getDatabasePlatform()) as $sql) {
+  print($sql . "\n");
+  $tmpConnection->executeStatement($sql);
+}
