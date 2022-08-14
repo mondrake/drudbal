9e70d51f92aca267f1af526192748e855c88ecd0
mondrake <mondrake.org@gmail.com>
2022-08-14 23:52:12 +0200

    Fix mysqli (#287)

--- src/Driver/Database/dbal/DbalExtension/MysqliExtension.php
+++ src/Driver/Database/dbal/DbalExtension/MysqliExtension.php
@@ -13,6 +13,29 @@ class MysqliExtension extends AbstractMySqlExtension {
   /**
    * {@inheritdoc}
    */
+  public function getDbServerPlatform(bool $strict = FALSE): string {
+    if (!$strict) {
+      return 'mysql';
+    }
+    $dbal_server_version = $this->getDbalConnection()->getNativeConnection()->get_server_info();
+    $regex = '/^(?:5\.5\.5-)?(\d+\.\d+\.\d+.*-mariadb.*)/i';
+    preg_match($regex, $dbal_server_version, $matches);
+    return (empty($matches[1])) ? 'mysql' : 'mariadb';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDbServerVersion(): string {
+    $dbal_server_version = $this->getDbalConnection()->getNativeConnection()->get_server_info();
+    $regex = '/^(?:5\.5\.5-)?(\d+\.\d+\.\d+.*-mariadb.*)/i';
+    preg_match($regex, $dbal_server_version, $matches);
+    return $matches[1] ?? $dbal_server_version;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function delegateClientVersion() {
     return mysqli_get_client_info();
   }
@@ -44,7 +67,7 @@ class MysqliExtension extends AbstractMySqlExtension {
    * {@inheritdoc}
    */
   public function delegateRowCount(DbalResult $dbal_result) {
-    $wrapped_connection = $this->getDbalConnection()->getNativeConnection()->getWrappedResourceHandle();
+    $wrapped_connection = $this->getDbalConnection()->getNativeConnection();
     if ($wrapped_connection->info === NULL) {
       return $dbal_result->rowCount();
     }
