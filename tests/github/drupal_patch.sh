a17ea3c78287c7466ac6576c73c9f8b05e931ab9
mondrake <mondrake.org@gmail.com>
2022-10-16 19:12:08 +0200

    Fix

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -10,5 +10,4 @@ curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git a
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/785.diff | git apply -v
 
 #3256642 Autoload classes of database drivers modules' dependencies
-#curl https://git.drupalcode.org/project/drupal/-/merge_requests/1626.diff | git apply -v
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/2844.diff | git apply -v
