88063148eb53a3cd74f7662b4c0bbd27271aefaa
mondrake <mondrake.org@gmail.com>
2022-10-21 15:38:54 +0200

    III

--- tests/src/Kernel/dbal/SchemaTest.php
+++ tests/src/Kernel/dbal/SchemaTest.php
@@ -6,6 +6,7 @@ use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Database\Database;
 use Drupal\drudbal\Driver\Database\dbal\Connection as DruDbalConnection;
+use Drupal\drudbal\Driver\Database\dbal\Schema as DruDbalSchema;
 use Drupal\KernelTests\Core\Database\DriverSpecificSchemaTestBase;
 
 /**
@@ -25,12 +26,21 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
   }
 
   /**
+   * Returns the DruDbal schema.
+   */
+  private function connection(): DruDbalSchema {
+    $schema = $this->schema;
+    assert($schema instanceof DruDbalSchema);
+    return $schema;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function checkSchemaComment(string $description, string $table, string $column = NULL): void {
-    $comment = $this->schema->getComment($table, $column);
+    $comment = $this->schema()->getComment($table, $column);
     // The schema comment truncation for mysql is different.
-    if ($this->connection->databaseType() === 'mysql') {
+    if ($this->connection()->databaseType() === 'mysql') {
       $max_length = $column ? 255 : 60;
       $description = Unicode::truncate($description, $max_length, TRUE, TRUE);
     }
@@ -41,9 +51,9 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
    * {@inheritdoc}
    */
   protected function assertCollation(): void {
-    if ($this->connection->databaseType() === 'mysql') {
+    if ($this->connection()->databaseType() === 'mysql') {
       // Make sure that varchar fields have the correct collations.
-      $columns = $this->connection->query('SHOW FULL COLUMNS FROM {test_table}');
+      $columns = $this->connection()->query('SHOW FULL COLUMNS FROM {test_table}');
       foreach ($columns as $column) {
         if ($column->Field == 'test_field_string') {
           $string_check = $column->Collation;
@@ -109,14 +119,14 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
         ],
       ],
     ];
-    $this->schema->createTable('test_table_index_length', $table_specification);
+    $this->schema()->createTable('test_table_index_length', $table_specification);
 
     // Ensure expected exception thrown when adding index with missing info.
     $expected_exception_message = "MySQL needs the 'test_field_text' field specification in order to normalize the 'test_regular' index";
     $missing_field_spec = $table_specification;
     unset($missing_field_spec['fields']['test_field_text']);
     try {
-      $this->schema->addIndex('test_table_index_length', 'test_separate', [['test_field_text', 200]], $missing_field_spec);
+      $this->schema()->addIndex('test_table_index_length', 'test_separate', [['test_field_text', 200]], $missing_field_spec);
       $this->fail('SchemaException not thrown when adding index with missing information.');
     }
     catch (SchemaException $e) {
@@ -124,13 +134,13 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
     }
 
     // Add a separate index.
-    $this->schema->addIndex('test_table_index_length', 'test_separate', [['test_field_text', 200]], $table_specification);
+    $this->schema()->addIndex('test_table_index_length', 'test_separate', [['test_field_text', 200]], $table_specification);
     $table_specification_with_new_index = $table_specification;
     $table_specification_with_new_index['indexes']['test_separate'] = [['test_field_text', 200]];
 
     // Ensure that the exceptions of addIndex are thrown as expected.
     try {
-      $this->schema->addIndex('test_table_index_length', 'test_separate', [['test_field_text', 200]], $table_specification);
+      $this->schema()->addIndex('test_table_index_length', 'test_separate', [['test_field_text', 200]], $table_specification);
       $this->fail('\Drupal\Core\Database\SchemaObjectExistsException exception missed.');
     }
     catch (SchemaObjectExistsException $e) {
@@ -138,7 +148,7 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
     }
 
     try {
-      $this->schema->addIndex('test_table_non_existing', 'test_separate', [['test_field_text', 200]], $table_specification);
+      $this->schema()->addIndex('test_table_non_existing', 'test_separate', [['test_field_text', 200]], $table_specification);
       $this->fail('\Drupal\Core\Database\SchemaObjectDoesNotExistException exception missed.');
     }
     catch (SchemaObjectDoesNotExistException $e) {
@@ -146,7 +156,7 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
     }
 
     // Get index information.
-    $results = $this->connection->query('SHOW INDEX FROM {test_table_index_length}');
+    $results = $this->connection()->query('SHOW INDEX FROM {test_table_index_length}');
     $expected_lengths = [
       'test_regular' => [
         'test_field_text' => 191,
@@ -184,62 +194,6 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
       $test_count++;
     }
     $this->assertEquals($column_count, $test_count, 'Number of tests matches expected value.');
-  }
-
-  /**
-   * @covers \Drupal\mysql\Driver\Database\mysql\Schema::introspectIndexSchema
-   */
-  public function testIntrospectIndexSchema(): void {
-    $table_specification = [
-      'fields' => [
-        'id'  => [
-          'type' => 'int',
-          'not null' => TRUE,
-          'default' => 0,
-        ],
-        'test_field_1'  => [
-          'type' => 'int',
-          'not null' => TRUE,
-          'default' => 0,
-        ],
-        'test_field_2'  => [
-          'type' => 'int',
-          'default' => 0,
-        ],
-        'test_field_3'  => [
-          'type' => 'int',
-          'default' => 0,
-        ],
-        'test_field_4'  => [
-          'type' => 'int',
-          'default' => 0,
-        ],
-        'test_field_5'  => [
-          'type' => 'int',
-          'default' => 0,
-        ],
-      ],
-      'primary key' => ['id', 'test_field_1'],
-      'unique keys' => [
-        'test_field_2' => ['test_field_2'],
-        'test_field_3_test_field_4' => ['test_field_3', 'test_field_4'],
-      ],
-      'indexes' => [
-        'test_field_4' => ['test_field_4'],
-        'test_field_4_test_field_5' => ['test_field_4', 'test_field_5'],
-      ],
-    ];
-
-    $table_name = strtolower($this->getRandomGenerator()->name());
-    $this->schema->createTable($table_name, $table_specification);
-
-    unset($table_specification['fields']);
-
-    $introspect_index_schema = new \ReflectionMethod(get_class($this->schema), 'introspectIndexSchema');
-    $introspect_index_schema->setAccessible(TRUE);
-    $index_schema = $introspect_index_schema->invoke($this->schema, $table_name);
-
-    $this->assertEquals($table_specification, $index_schema);
   }
 
   /**
@@ -290,13 +244,13 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
     ];
 
     $table_name = strtolower($this->getRandomGenerator()->name());
-    $this->schema->createTable($table_name, $table_specification);
+    $this->schema()->createTable($table_name, $table_specification);
 
     unset($table_specification['fields']);
 
     $introspect_index_schema = new \ReflectionMethod(get_class($this->schema), 'introspectIndexSchema');
-    $introspect_index_schema->setAccessible(TRUE);
-    $index_schema = $introspect_index_schema->invoke($this->schema, $table_name);
+    $introspect_index_schema()->setAccessible(TRUE);
+    $index_schema = $introspect_index_schema()->invoke($this->schema, $table_name);
 
     // Oracle is using a custom naming scheme for its indexes, so
     // we need to adjust the initial table specification.
@@ -377,11 +331,11 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
         ],
       ],
     ];
-    $this->schema->createTable($table_name, $table_specification);
+    $this->schema()->createTable($table_name, $table_specification);
 
-    $this->assertTrue($this->schema->tableExists($table_name), 'Table with uppercase table name exists');
-    $this->assertContains($table_name, $this->schema->findTables('%'));
-    $this->assertTrue($this->schema->dropTable($table_name), 'Table with uppercase table name dropped');
+    $this->assertTrue($this->schema()->tableExists($table_name), 'Table with uppercase table name exists');
+    $this->assertContains($table_name, $this->schema()->findTables('%'));
+    $this->assertTrue($this->schema()->dropTable($table_name), 'Table with uppercase table name dropped');
   }
 
   /**
@@ -491,7 +445,7 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
       'fields' => ['serial_column' => ['type' => 'serial', 'unsigned' => TRUE, 'not null' => TRUE]],
       'primary key' => ['serial_column'],
     ];
-    $this->schema->createTable($table_name, $table_spec);
+    $this->schema()->createTable($table_name, $table_spec);
 
     // Now set up columns for the other types.
     $types = ['int', 'float', 'numeric'];
@@ -502,12 +456,12 @@ class SchemaTest extends DriverSpecificSchemaTestBase {
       }
       $column_name = $type . '_column';
       $table_spec['fields'][$column_name] = $column_spec;
-      $this->schema->addField($table_name, $column_name, $column_spec);
+      $this->schema()->addField($table_name, $column_name, $column_spec);
     }
 
     // Finally, check each column and try to insert invalid values into them.
     foreach ($table_spec['fields'] as $column_name => $column_spec) {
-      $this->assertTrue($this->schema->fieldExists($table_name, $column_name), new FormattableMarkup('Unsigned @type column was created.', ['@type' => $column_spec['type']]));
+      $this->assertTrue($this->schema()->fieldExists($table_name, $column_name), new FormattableMarkup('Unsigned @type column was created.', ['@type' => $column_spec['type']]));
       if ($column_name !== 'serial_column') {
         $this->assertFalse($this->tryUnsignedInsert($table_name, $column_name), new FormattableMarkup('Unsigned @type column rejected a negative value.', ['@type' => $column_spec['type']]));
       }
