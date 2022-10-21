b0bce414156224871377f36866781dab2e695277
mondrake <mondrake.org@gmail.com>
2022-10-21 23:15:46 +0200

    Readd test limitations

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -4,7 +4,7 @@
 curl https://www.drupal.org/files/issues/2022-06-01/2992274-25.patch | git apply -v
 
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
-#curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
 
 #3191623 Views aggregate queries do not escape the fields
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/785.diff | git apply -v
