42601dec2d203640cdae6b43da6e15a6e8d7475b
mondrake <mondrake.org@gmail.com>
2022-10-22 18:24:08 +0200

    VI

--- src/Driver/Database/dbal/Connection.php
+++ src/Driver/Database/dbal/Connection.php
@@ -498,6 +498,7 @@ class Connection extends DatabaseConnection {
     if (!empty($this->rootTransactionEndCallbacks)) {
       $callbacks = $this->rootTransactionEndCallbacks;
       $this->rootTransactionEndCallbacks = [];
+dump(['** CALLBACKS', $callbacks]);
       foreach ($callbacks as $callback) {
         call_user_func($callback, $success);
       }
