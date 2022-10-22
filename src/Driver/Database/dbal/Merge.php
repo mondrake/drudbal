f458232761c8db2f238899f6c2bc5b9948d3f3d6
mondrake <mondrake.org@gmail.com>
2022-10-22 20:55:54 +0200

    XIII

--- src/Driver/Database/dbal/Merge.php
+++ src/Driver/Database/dbal/Merge.php
@@ -22,7 +22,6 @@ class Merge extends QueryMerge {
    * {@inheritdoc}
    */
   public function __construct(Connection $connection, string $table, array $options = []) {
-global $xxx; if ($xxx) dump(['merge:__construct', $table, $options]);
     // @todo Remove the __construct in Drupal 11.
     // @see https://www.drupal.org/project/drupal/issues/3256524
     parent::__construct($connection, $table, $options);
--- src/Driver/Database/dbal/Select.php
+++ src/Driver/Database/dbal/Select.php
@@ -19,6 +19,7 @@ class Select extends QuerySelect {
     * {@inheritdoc}
     */
    public function __construct(Connection $connection, $table, $alias = NULL, array $options = []) {
+global $xxx; if ($xxx) dump(['select:__construct', $table, $alias]);
      // @todo Remove the __construct in Drupal 11.
      // @see https://www.drupal.org/project/drupal/issues/3256524
      parent::__construct($connection, $table, $alias, $options);
