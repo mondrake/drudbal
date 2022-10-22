aa6a924b4ae4fbf44433249850a7d341faa1aced
mondrake <mondrake.org@gmail.com>
2022-10-22 19:34:08 +0200

    IX

--- src/Driver/Database/dbal/Merge.php
+++ src/Driver/Database/dbal/Merge.php
@@ -18,6 +18,7 @@ class Merge extends QueryMerge {
    * {@inheritdoc}
    */
   public function __construct(Connection $connection, string $table, array $options = []) {
+global $xxx; if ($xxx) dump(['merge:__construct', $table, $options]);
     // @todo Remove the __construct in Drupal 11.
     // @see https://www.drupal.org/project/drupal/issues/3256524
     parent::__construct($connection, $table, $options);
