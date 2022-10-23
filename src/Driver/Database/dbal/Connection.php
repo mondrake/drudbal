8b6c44222632c20c23b749d54f144a46f51338c7
mondrake <mondrake.org@gmail.com>
2022-10-23 22:06:37 +0200

    VIII

--- src/Driver/Database/dbal/Connection.php
+++ src/Driver/Database/dbal/Connection.php
@@ -386,7 +386,7 @@ class Connection extends DatabaseConnection {
    * {@inheritdoc}
    */
   public function rollBack($savepoint_name = 'drupal_transaction') {
-global $xxx; if ($xxx) dump(['rollBack', $savepoint_name, $this->transactionDepth(), array_reverse($this->transactionLayers), Error::formatBacktrace(debug_backtrace())]);
+//global $xxx; if ($xxx) dump(['rollBack', $savepoint_name, $this->transactionDepth(), array_reverse($this->transactionLayers), Error::formatBacktrace(debug_backtrace())]);
     if (!$this->inTransaction()) {
       throw new TransactionNoActiveException();
     }
@@ -437,7 +437,7 @@ global $xxx; if ($xxx) dump(['rollBack', $savepoint_name, $this->transactionDept
    * {@inheritdoc}
    */
   public function pushTransaction($name) {
-global $xxx; if ($xxx) dump(['pushTransaction', $this->transactionDepth(), $name, array_reverse($this->transactionLayers)]);
+//global $xxx; if ($xxx) dump(['pushTransaction', $this->transactionDepth(), $name, array_reverse($this->transactionLayers)]);
     if (isset($this->transactionLayers[$name])) {
       throw new TransactionNameNonUniqueException($name . " is already in use.");
     }
@@ -456,7 +456,7 @@ global $xxx; if ($xxx) dump(['pushTransaction', $this->transactionDepth(), $name
    * {@inheritdoc}
    */
   protected function popCommittableTransactions() {
-global $xxx; if ($xxx) dump(['popCommittableTransactions', $this->transactionDepth(), array_reverse($this->transactionLayers)]);
+//global $xxx; if ($xxx) dump(['popCommittableTransactions', $this->transactionDepth(), array_reverse($this->transactionLayers)]);
     // Commit all the committable layers.
     foreach (array_reverse($this->transactionLayers) as $name => $active) {
 //if ($xxx) dump(['popCommittableTransactions:1', $this->transactionLayers, $name, $active]);
@@ -496,13 +496,13 @@ global $xxx; if ($xxx) dump(['popCommittableTransactions', $this->transactionDep
    * @internal
    */
   protected function doCommit() {
-global $xxx; if ($xxx) dump(['doCommit']);
+//global $xxx; if ($xxx) dump(['doCommit']);
     try {
       $this->getDbalExtension()->delegateCommit();
       $success = TRUE;
     }
     catch (DbalConnectionException $e) {
-if ($xxx) dump(['doCommit:exc'], $e);
+//if ($xxx) dump(['doCommit:exc'], $e);
       $success = FALSE;
     }
 
