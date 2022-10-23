c6a042605264b33ad154532674692049f8d5513a
mondrake <mondrake.org@gmail.com>
2022-10-23 15:05:14 +0200

    XII

--- tests/src/Kernel/dbal/EndOfTransactionQueriesTest.php
+++ tests/src/Kernel/dbal/EndOfTransactionQueriesTest.php
@@ -20,6 +20,7 @@ class EndOfTransactionQueriesTest extends DriverSpecificEndOfTransactionQueriesT
     // Override default database driver to one that does logging. Third-party
     // (non-core) database drivers can achieve the same test coverage by
     // subclassing this test class and overriding only this method.
+    //
     // @see \Drupal\database_statement_monitoring_test\LoggedStatementsTrait
     // @see \Drupal\drudbal\StatementMonitoring\dbal\Connection
     $info['default']['namespace'] = '\Drupal\drudbal\StatementMonitoring\dbal';
