e25d460031478895d1b2c3d789277f978a0d599f
mondrake <mondrake.org@gmail.com>
2022-10-23 18:42:44 +0200

    XXIV

--- tests/src/Kernel/dbal/EndOfTransactionQueriesTest.php
+++ tests/src/Kernel/dbal/EndOfTransactionQueriesTest.php
@@ -39,7 +39,7 @@ if ($xxx) dump(['statementToTableName:1', $matches]);
     }
     elseif (preg_match('/.*FROM [\"\`](' . $prefix . ')([a-z0-9]*)[\"\`].*/', $statement, $matches)) {
 if ($xxx) dump(['statementToTableName:2', $matches]);
-      return $matches[1];
+      return $matches[2];
     }
     elseif (preg_match('/.*FROM [\"\`]([a-z0-9]*)[\"\`].*/', $statement, $matches)) {
 if ($xxx) dump(['statementToTableName:3', $matches]);
