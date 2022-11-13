26901ccc1d0f4808ebc37418273720e055a88b15
mondrake <mondrake.org@gmail.com>
2022-11-13 19:08:49 +0100

    XIII

--- src/Driver/Database/dbal/Transaction.php
+++ src/Driver/Database/dbal/Transaction.php
@@ -4,34 +4,16 @@ namespace Drupal\drudbal\Driver\Database\dbal;
 
 use Drupal\Core\Database\Transaction as TransactionBase;
 
-/**
- * DruDbal implementation of \Drupal\Core\Database\Query\Merge.
- *
- * Note: there should not be db platform specific code here. Any tasks that
- * cannot be managed by Doctrine DBAL should be added to extension specific
- * code in Drupal\drudbal\Driver\Database\dbal\DbalExtension\[dbal_driver_name]
- * classes and execution handed over to there.
- */
 class Transaction extends TransactionBase {
 
   public function __construct(Connection $connection, $name = NULL) {
-dump([__METHOD__, 1, $connection->transactionDepth(), \Drupal\Core\Utility\Error::formatBacktrace(debug_backtrace())]);
+dump([__METHOD__, 1, $connection->transactionDepth(), $connection->getDbalConnection()->getNativeConnection()->inTransaction(), \Drupal\Core\Utility\Error::formatBacktrace(debug_backtrace())]);
     parent::__construct($connection, $name);
   }
 
   public function __destruct() {
-dump([__METHOD__, 1, $this->connection->transactionDepth(), \Drupal\Core\Utility\Error::formatBacktrace(debug_backtrace())]);
+dump([__METHOD__, 1, $this->connection->transactionDepth(), $this->connection->getDbalConnection()->getNativeConnection()->inTransaction(), \Drupal\Core\Utility\Error::formatBacktrace(debug_backtrace())]);
     parent::__destruct();
-  }
-
-  public function name() {
-dump([__METHOD__, 1]);
-    return parent::name();
-  }
-
-  public function rollBack() {
-dump([__METHOD__, 1, $this->connection->transactionDepth(), \Drupal\Core\Utility\Error::formatBacktrace(debug_backtrace())]);
-    return parent::rollBack();
   }
 
 }
