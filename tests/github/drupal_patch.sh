685b6e1fb83a01e74de51c7aa0ddd6345f2c8c29
mondrake <mondrake.org@gmail.com>
2023-03-08 21:24:42 +0100

    I

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -8,3 +8,6 @@ curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git a
 
 #3256642 Autoload classes of database drivers modules' dependencies
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/2844.diff | git apply -v
+
+#3265086 Implement statement classes using \Iterator to fix memory usage regression and prevent rewinding
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/1842.diff | git apply -v
