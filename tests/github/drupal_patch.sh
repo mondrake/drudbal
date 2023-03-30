2067ebad31c50f05fa5110570150a502a45e6973
mondrake <mondrake.org@gmail.com>
2023-03-30 19:25:45 +0200

    Local patch for #2992274 Installer tests fail if contrib driver hides database credentials form fields (#319)

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 
-#2992274 Installer tests fail if contrib driver hides database credentials form fields
-# curl https://www.drupal.org/files/issues/2022-06-01/2992274-25.patch | git apply -v
-
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
 
 #3256642 Introduce database driver extensions and autoload database drivers' dependencies
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/3169.diff | git apply -v
+
+#2992274 Installer tests fail if contrib driver hides database credentials form fields
+# curl https://www.drupal.org/files/issues/2022-06-01/2992274-25.patch | git apply -v
+git apply -v ./drudbal_staging/tests/github/2992274-local.patch
 
 #3347497 Introduce a FetchModeTrait to allow emulating PDO fetch modes
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/3676.diff | git apply -v
