aec3ffa43994b8eabc4f63e2ed8bbef435bb121b
mondrake <mondrake.org@gmail.com>
2023-06-16 22:03:25 +0200

    adjust post #3256642

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -3,9 +3,6 @@
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
 
-#3256642 Introduce database driver extensions and autoload database drivers' dependencies
-curl https://git.drupalcode.org/project/drupal/-/merge_requests/3169.diff | git apply -v
-
 #2992274 Installer tests fail if contrib driver hides database credentials form fields
 # curl https://www.drupal.org/files/issues/2022-06-01/2992274-25.patch | git apply -v
 git apply -v ./drudbal_staging/tests/github/2992274-local.patch
