44d9c753df79e989212c2df4ffe1f8c880e476ee
mondrake <mondrake.org@gmail.com>
2023-08-04 12:33:33 +0200

    i

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -6,3 +6,6 @@ curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git a
 #2992274 Installer tests fail if contrib driver hides database credentials form fields
 # curl https://www.drupal.org/files/issues/2022-06-01/2992274-25.patch | git apply -v
 git apply -v ./drudbal_staging/tests/github/2992274-local.patch
+
+#3375959 Add a way to delay executions in test runner until terminate event completed in the child site
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/4461.diff | git apply -v
